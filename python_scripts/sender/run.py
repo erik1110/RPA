791330a84176fbf61c164d69e62407f4034a40f3
erik1110 <oceanuheart@gmail.com>
2021-02-25 18:10:13 +0800

    [debug] 調整sender副檔名錯誤

--- python_scripts/sender/run.py
+++ python_scripts/sender/run.py
@@ -49,5 +49,5 @@ if __name__ == '__main__':
   context = '${{ github.job }} job in worflow ${{ github.workflow }} of ${{ github.repository }} has ${{ job.status }}'
   recipents = '${{secrets.MAIL_ADDRESS}}'
   folder_path = 'data'
-  filename = 'result.pdf'
+  filename = 'result.xlsx'
   sendmail3(email_user, email_pwd, subject, context, recipents, folder_path, filename)
